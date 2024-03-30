0d9be54641a59cab393ce2639194d05805cf4ad8
Mafuyeka <tshepisoroshina@gmail.com>
2024-03-30 10:26:23 -0700

    Updated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL with contents of a file in the body
-# It then displays the body of the response
-file_content=$(cat "$2" 2>/dev/null)
-if [[ $? -ne 0 ]]; then
-echo "Not a valid JSON"
-exit 1
-fi
-curl -sX POST -H "Content-Type: application/json" -d "$file_content" "$1"
+# Script sends a JSON POST request to a URL first argument, displays the body of response
+curl -sX POST -d "@$2" -H "Content-Type: application/json" "$1"

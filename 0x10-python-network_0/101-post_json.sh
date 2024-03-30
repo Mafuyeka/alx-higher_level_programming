72b5d89b054b4ffb219f74418896f95eac6f6704
Mafuyeka <tshepisoroshina@gmail.com>
2024-03-30 05:33:08 -0700

    Updated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -3,8 +3,7 @@
 # It then displays the body of the response
 file_content=$(cat "$2" 2>/dev/null)
 if [[ $? -ne 0 ]]; then
-    echo "Not a valid JSON"
-    exit 1
+echo "Not a valid JSON"
+exit 1
 fi
-
 curl -sX POST -H "Content-Type: application/json" -d "$file_content" "$1"
